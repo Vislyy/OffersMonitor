383da36f10b5fb09ef9844e4215d142c9617a045
Visslyx <maksimslepuch@gmail.com>
2025-07-30 13:13:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,21 @@ window = QWidget()
 
 mainline = QVBoxLayout()
 
+query_lbl = QLabel("Введіть пошуковий запит")
+query_text = QLineEdit()
+price_from_lbl = QLabel("Введіть ціну від якої потрібно шукати (0-10000000)")
+price_from_text = QLineEdit()
+price_to_lbl = QLabel("Введіть ціну від якої потрібно шукати (До 10000000)")
+price_to_text = QLineEdit()
 
+mainline.addWidget(query_lbl)
+mainline.addWidget(query_text)
+mainline.addWidget(price_from_lbl)
+mainline.addWidget(price_from_text)
+mainline.addWidget(price_to_lbl)
+mainline.addWidget(price_to_text)
+
+window.setLayout(mainline)
 
 window.show()
 app.exec()
