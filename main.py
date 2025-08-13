e960e4cb946cfca134eb0073c37941889afb421d
Veronika648 <maksimslepuch@gmail.com>
2025-08-13 14:30:25 +0300

    Ready to work

--- main.py
+++ main.py
@@ -44,4 +44,4 @@ if __name__ == "__main__":
         window.show()
         app.exec()
     except KeyboardInterrupt:
-        print("[!] Programm was closed")
+        print("[!] Programm was forcefully closed")
